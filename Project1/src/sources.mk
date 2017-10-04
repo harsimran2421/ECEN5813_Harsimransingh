b591483bcd1b9cc89d2753e12f010b03f55b2770
Smitesh07 <Smitesh.Modak@colorado.edu>
2017-10-03 23:19:27 -0600

    Addition in regards to Linker

--- Project1/src/sources.mk
+++ Project1/src/sources.mk
@@ -5,9 +5,11 @@ SRCS = main.c \
        debug.c \
 
 LIBR = memory.c \
-			 conversion.c\
+       conversion.c\
 
 INCLUDE_H = ../includes/common/
 INCLUDE_H_FF = ../includes/kl25z/
 INCLUDE_H_FF1 = ../includes/CMSIS/
+INCLUDE_LINKER = ../platform/MKL25Z128xxx4_flash.ld
+
 vpath %.h $(INCLUDE_H)
